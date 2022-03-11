88da6ab62e66e7d252d0acd720174e51f35f28b5
Rand-783 <77523512+Rand-783@users.noreply.github.com>
2022-03-10 22:42:47 -0500

    done

---
+++ script.js
@@ -0,0 +1,34 @@
+const $content= document.getElementById('content')
+const $popup= document.getElementById('popup')
+
+const images = [
+    'flower1jpg',
+    'flower2',
+    'flower3',
+    'flower4',
+    'flower5',
+    'flower6',
+    'flower7',
+    'flower8',
+    'flower9',
+    'flower10',
+    'flower11',
+    'flower12',
+]
+
+for (let i = 0; i < 12; i++){
+    $content.innerHTML += `<img src='images/${images[i]}.jpg'>`
+
+}
+
+$content.addEventListener('click', function(e){
+    if(e.target.tagName == 'IMG') { 
+    $popup.innerHTML = `<img src=${e.target.getAttribute('src')}
+    >`
+    $popup.classList.remove('hide')
+}})
+
+$popup.addEventListener('click', function () {
+    $popup.classList.add('hide')
+
+})
